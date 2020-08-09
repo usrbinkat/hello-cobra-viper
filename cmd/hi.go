5a38798dd0d62a3712d4a61618795839f8e711dc
usrbinkat <kat@braincraft.io>
2020-08-09 21:04:07 +0000

    testing

--- cmd/hi.go
+++ cmd/hi.go
@@ -10,21 +10,29 @@ var hiCmd = &cobra.Command{
     Use:   "hi",
     Short: "Cobra & Viper Golang Hello Demo",
     Long: "",
-    Run: func(cmd *cobra.Command, args []string) {
-    },
 }
 
 var (
     greeting string
 )
 
+
+
 func init() {
+    hiCmd := &cobra.Command{
+        Use: "hi --greeting 'how are you today?'",
+        Short: "Say Hello!",
+    Run: func(cmd *cobra.Command, args []string) {
+        CoreHello()
+    },
+}
+
     rootCmd.AddCommand(hiCmd)
     hiCmd.PersistentFlags().StringVarP(&greeting "greeting", "g", "How are you?", "Greeting statement")
 }
 
 func CoreHello() {
-
+    fmt.Println(greeting)
     if err := rootCmd.Execute(); err != nil {
         fmt.Println(err)
         os.Exit(1)
