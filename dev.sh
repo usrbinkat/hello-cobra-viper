71fa16ac25c113d5863fa7d72c84850369579d4f
usrbinkat <kat@braincraft.io>
2020-08-09 20:32:27 +0000

    testing

--- dev.sh
+++ dev.sh
@@ -4,5 +4,5 @@ rm -f dev 2>/dev/null
 sudo chown $USER ~/.ssh -R
 git stage -A; git commit -m 'testing'; git push origin master
 ./build.sh
-cp -f ./bin/gohi /usr/bin/gohi 2>/dev/null
-cp -f ./dev /usr/bin/gohi 2>/dev/null
+cp -f ./bin/higo /usr/bin/higo 2>/dev/null
+cp -f ./dev /usr/bin/higo 2>/dev/null
